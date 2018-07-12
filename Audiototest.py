f60658d69105b8afc6c8c3aed2eec082b4380232
singhsachin1312 <40062792+singhsachin1312@users.noreply.github.com>
2018-07-12 16:32:10 +0530

    Add files via upload

---
+++ Audiototest.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+print(sr.__version__)
+r=sr.Recognizer()
+harvard = sr.AudioFile('02 - Dilbar Dilbar - Neha Kakkar Full Mp3 Songs(Zgana.in).wav')
+with harvard as source:
+    audio = r.record(source)
+    print(type(audio))
+    print(r.recognize_google(audio,language='hi-HI'))
+    print(r.recognize_google(audio))
