34378fa7af96feb7551d53fccd359980b9011c25
singhsachin1312 <40062792+singhsachin1312@users.noreply.github.com>
2018-07-12 16:46:00 +0530

    Update Audiototest.py

--- Audiototest.py
+++ Audiototest.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 print(sr.__version__)
 r=sr.Recognizer()
-harvard = sr.AudioFile('02 - Dilbar Dilbar - Neha Kakkar Full Mp3 Songs(Zgana.in).wav')
+harvard = sr.AudioFile('02 - Dilbar Dilbar - Neha Kakkar Full Mp3 Songs(Zgana.in).wav')   #wav not mp3
 with harvard as source:
     audio = r.record(source)
     print(type(audio))
