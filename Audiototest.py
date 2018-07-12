7ccbbc988fbe5f6abf114f09bb825989b1800c33
singhsachin1312 <40062792+singhsachin1312@users.noreply.github.com>
2018-07-12 16:48:36 +0530

    Delete Audiototest.py

--- Audiototest.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-print(sr.__version__)
-r=sr.Recognizer()
-harvard = sr.AudioFile('02 - Dilbar Dilbar - Neha Kakkar Full Mp3 Songs(Zgana.in).wav')   #wav not mp3
-with harvard as source:
-    audio = r.record(source)
-    print(type(audio))
-    print(r.recognize_google(audio,language='hi-HI'))
-    print(r.recognize_google(audio))
